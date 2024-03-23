3db4109c6c611d2efc89bb384732be29f782d840
J/ <55988027+jkerai1@users.noreply.github.com>
2024-03-23 09:21:54 +0000

    Update serverless.py

--- serverless.py
+++ serverless.py
@@ -27,6 +27,8 @@ def pixel():
         #Teams Webhook
         #myTeamsMessage.text(f"[*] Requester IP (user logging in): {requester_ip} & Referer header (AitM): {referer_header}")
         #myTeamsMessage.send()
+    else: 
+        filename = "safe.png"
     return send_file(filename, mimetype='image/png',as_attachment=False)
 
 def main():
