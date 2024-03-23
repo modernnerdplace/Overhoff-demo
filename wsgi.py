7315d463161fd7d391546e01a2d7092fe67aa83d
J/ <55988027+jkerai1@users.noreply.github.com>
2024-03-23 08:16:50 +0000

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+#For serverless 
+
+import sys
+
+# add your project directory to the sys.path
+project_home = '/home/TempDef/mysite'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+# import flask app but need to call it "application" for WSGI to work
+from flask_app import app as application  # noqa
