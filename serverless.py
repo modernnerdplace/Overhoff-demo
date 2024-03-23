a43ab3d709a09108b350eee3beaaaeef6cf62a6c
J/ <55988027+jkerai1@users.noreply.github.com>
2024-03-23 11:38:00 +0000

    Update serverless.py

--- serverless.py
+++ serverless.py
@@ -13,18 +13,18 @@ allowed_referers = [
         'tasks.office.com',
         'login.windows.net']
 app = Flask(__name__)
-filename = "warning.png"
+#filename = "warning.png"
 
 @app.route(f'/{pixel_filename}')
 def pixel():
     #requester_ip = request.remote_addr  #To fix.
     referer_header = str(request.headers.get('Referer'))   
     referer_header = referer_header.replace("https://","").replace("/","")
-    if (referer_header not in allowed_referers and !(referer_header == None) or referer_header =="-"):
+    if (referer_header not in allowed_referers and !(referer_header == None)) or referer_header =="-":
         print(f"[!] Non-Microsoft referer header detected: {referer_header}")
         print(f"[*] Referer header (AitM): {referer_header}")
         #print(f"[*] Requester IP (user logging in): {requester_ip}")    #To Fix.
-        #filename = "warning.png"
+        filename = "warning.png"
         #Teams Webhook#
         #myTeamsMessage.text(f"[*] Requester IP (user logging in): {requester_ip} & Referer header (AitM): {referer_header}")
         #myTeamsMessage.send()
